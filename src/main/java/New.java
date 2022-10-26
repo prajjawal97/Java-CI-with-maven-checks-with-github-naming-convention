fb25a293731c3d1555a65c9ef0f5d95644c9a027
devanshGoyan <93072780+devanshGoyan@users.noreply.github.com>
2022-10-26 17:14:50 +0530

    Update New.java

--- src/main/java/New.java
+++ src/main/java/New.java
@@ -4,8 +4,9 @@ public class New {
     public static void main(String args[]){
         try {
             int i = 100/0;
+            int j= 240/i;
         } catch (Exception e) {
-            e.printStackTrace();
+            e.exception();
         }
     }
 }
